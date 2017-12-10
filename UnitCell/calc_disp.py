1183cccb678b551d16900a7eb4f9fc4e1ba20658
jeffwdoak <jeff.w.doak@gmail.com>
2017-12-10 10:17:36 -0600

    cleaned up calc_disp.py

--- UnitCell/calc_disp.py
+++ UnitCell/calc_disp.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python
 
-from unitcell import *
+"""
+Calculate the displacement between two unitcells in Cartesian coordinates.
+
+Command-line arguments
+----------------------
+1) Path to first unitcell to compare
+2) Path to second unitcell to compare
+3) Whether or not to calculate displacement vectors. If any 3rd argument is
+   present, displacement vectors will be calculated, otherwise, only
+   displacement magnitudes will be calculated.
+
+"""
+
 import sys
 
-cell_1 = UnitCell(open(str(sys.argv[1]),'r'))
-cell_2 = UnitCell(open(str(sys.argv[2]),'r'))
+from unitcell import UnitCell, displacements
+
+cell_1 = UnitCell(open(str(sys.argv[1]), 'r'))
+cell_2 = UnitCell(open(str(sys.argv[2]), 'r'))
 
 #disp = displacements(cell_1,cell_2,conv='C')
 #disp = np.round(disp,decimals=9)
-mag = displacements(cell_1,cell_2,conv='C',flag=False)
-#mag,dir = displacements(cell_1,cell_2,conv='C',flag=True)
-print mag
-#print dir
+if len(sys.argv) > 2:
+    mag, dir_ = displacements(cell_1, cell_2, conv='C', flag=True)
+    print mag
+    print dir_
+else:
+    mag = displacements(cell_1, cell_2, conv='C', flag=False)
+    print mag
