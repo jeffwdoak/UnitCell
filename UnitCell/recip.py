b54ff0390acef4bf917b49d7f9f4dbed5d71c59f
jeffwdoak <jeff.w.doak@gmail.com>
2017-12-10 09:43:04 -0600

    cleaned up recip.py

--- UnitCell/recip.py
+++ UnitCell/recip.py
@@ -1,11 +1,18 @@
-#!/usr/bin/python
-from unitcell import *
+#!/usr/bin/env python
+
+"""
+Script to calculate and print to stdout the reciprocal-space lattice vectors of
+a unitcell.
+
+"""
+
 import sys
 
-file = sys.argv[1]
-cell_file = open(file,'r')
-cell = UnitCell(cell_file)
-recip,b = cell.recip_lat()
+from unitcell import UnitCell
+
+with open(sys.argv[1], 'r') as cell_file:
+    cell = UnitCell(cell_file)
+recip, b = cell.recip_lat()
 print recip
 print
 print b
