a4f6645d22b3304f691085e3df9bbcdd832bd481
jeffwdoak <jeff.w.doak@gmail.com>
2017-12-10 09:55:06 -0600

    cleaned up calc_com.py

--- UnitCell/calc_com.py
+++ UnitCell/calc_com.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
-from unitcell import *
+"""
+Calculate center of mass of atomic positions (and velocities, if present) for a
+unitcell.
+
+"""
+
 import sys
 
-cell = UnitCell(open(sys.argv[1],'r'))
+from unitcell import UnitCell
+
+cell = UnitCell(open(sys.argv[1], 'r'))
 if cell.atom_velocities.any():
-    r_com,v_com = cell.center_of_mass(conv="d",vel=True)
+    r_com, v_com = cell.center_of_mass(conv="d", vel=True)
     print "r_com"
     print r_com
     print "v_com"
